73d3b2a7291026639d09174173d6d468f1de1bf3
derwacter3 <derwacter3@fullsail.edu>
2014-06-12 17:04:57 -0400

    Update Derwacter_Joshua_Assignment2.js

--- sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a/Derwacter_Joshua_Assignment2.js
+++ sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a/Derwacter_Joshua_Assignment2.js
@@ -3,3 +3,11 @@
 // Joshua Derwacter
 // SDI 1406
 // Project 2
+
+// Variables
+var buildingOne = "Pylon";
+var buildingTwo = "Assimilator";
+var buildingThree = "Photon Cannon";
+var pylonTime = 30;
+var assimilatorTime = 40;
+var photonCannonTime = 50;
