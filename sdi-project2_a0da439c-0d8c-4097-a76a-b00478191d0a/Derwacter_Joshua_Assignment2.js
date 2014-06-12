67ca2bec079848aefbf1b228d18b8fc88c5b35e1
derwacter3 <derwacter3@fullsail.edu>
2014-06-12 18:09:05 -0400

    Update Derwacter_Joshua_Assignment2.js

--- sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a/Derwacter_Joshua_Assignment2.js
+++ sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a/Derwacter_Joshua_Assignment2.js
@@ -11,3 +11,33 @@ var buildingThree = "Photon Cannon";
 var pylonTime = 30;
 var assimilatorTime = 40;
 var photonCannonTime = 50;
+
+var multiplicationFunction = function() {
+	
+	totalTime = pylonTime * 5
+	
+	console.log("The total time for five " + buildingOne +  " will be " + totalTime + " seconds.");
+	
+};
+
+multiplicationFunction();
+
+var assimilatorFunction = function() {
+	
+	totalTime = assimilatorTime * 2
+	
+	console.log("The total time for two " + buildingTwo + " will be " + totalTime + " seconds.");
+	
+};
+
+assimilatorFunction();
+
+var photonCannonFunction = function() {
+
+	totalTime = photonCannonTime * 10
+	
+	console.log("The total time for ten " + buildingThree + " will be " + totalTime + " seconds.");
+	
+};
+
+photonCannonFunction();
