ced372ab07fdf5ae6833ef932f710f86913e0e3c
derwacter3 <derwacter3@fullsail.edu>
2014-06-12 18:50:54 -0400

    Update Derwacter_Joshua_Assignment2.js

--- sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a/Derwacter_Joshua_Assignment2.js
+++ sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a/Derwacter_Joshua_Assignment2.js
@@ -12,32 +12,54 @@ var pylonTime = 30;
 var assimilatorTime = 40;
 var photonCannonTime = 50;
 
+// Functions
 var multiplicationFunction = function() {
 	
-	totalTime = pylonTime * 5
+	totalTimePylon = pylonTime * 5
 	
-	console.log("The total time for five " + buildingOne +  " will be " + totalTime + " seconds.");
+	console.log("The total time for five " + buildingOne +  " will be " + totalTimePylon + " seconds.");
 	
 };
+
+var whileLoop = function(pylons){
+	
+	while(pylons > 0) {
+		
+		console.log("Pylons have this much time left " + totalTimePylon);
+	}
 
 multiplicationFunction();
 
 var assimilatorFunction = function() {
 	
-	totalTime = assimilatorTime * 2
+	totalTimeAssimilator = assimilatorTime * 2
 	
-	console.log("The total time for two " + buildingTwo + " will be " + totalTime + " seconds.");
+	console.log("The total time for two " + buildingTwo + " will be " + totalTimeAssimilator + " seconds.");
 	
+};
+
+var whileloop = function(assimilators) {
+	
+	while(assimilators > 0) {
+		
+		console.log("Assimilators have this much time left " + totalTimeAssimilator)
 };
 
 assimilatorFunction();
 
 var photonCannonFunction = function() {
 
-	totalTime = photonCannonTime * 10
+	totalTimePhotonCannon = photonCannonTime * 10
 	
-	console.log("The total time for ten " + buildingThree + " will be " + totalTime + " seconds.");
+	console.log("The total time for ten " + buildingThree + " will be " + totalTimePhotonCannon + " seconds.");
 	
+};
+
+var whileloop = function(photoncannons) {
+
+	while(photoncannons > 0) {
+		
+		console.log("Photon cannons have this much time left" + totalTimePhotonCannon)
 };
 
 photonCannonFunction();
